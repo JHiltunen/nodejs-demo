679113dc03efb6275c5ca75924e0bef02c123139
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-03-19 11:36:30 +0200

    separate MVC, example of query parameter (http://a.com?param=value&more=fun)

--- routes/catRouter.js
+++ routes/catRouter.js
@@ -1,12 +1,10 @@
 //'use strict'; module is strict by default
 const express = require('express');
 const router = express.Router();
+const catController = require('../controllers/catController');
 
 router.route('/')
-    .get((req, res) => {
-    console.log('get all cats')
-    res.send('Hello Cat!');
-    })
+    .get(catController.cat_list_get)
 
     .post((req, res) => {
         console.log('post cat');
