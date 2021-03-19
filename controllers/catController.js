a64eb254096cd85bfb778778b2ff816d4c80886d
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-03-19 16:09:52 +0200

    order by query

--- controllers/catController.js
+++ controllers/catController.js
@@ -7,14 +7,18 @@ const cats = catModel.cats;
 const cat_list_get = async (req, res) => {
     console.log('get all cats from controllers', req.query);
     if (req.query.sort == 'age') {
-        //const catsSort = cats.slice().sort((catA, catB) => catA.age - catB.age)
-        res.json({todo: 'will do later'});
+        const catsSort = await catModel.getAllCatsSort('age');
+        res.json(catsSort);
+        return;
+    } else if (req.query.sort === 'name') {
+        const catsSort = await catModel.getAllCatsSort('name');
+        res.json(catsSort);
         return;
     }
+    
     const cats = await catModel.getAllCats();
-    res.json(cats);
-};
-
+    res.json(cats);   
+}
 const cat_get_by_id = (req, res) => {
     console.log('get one cat by id', req.params);
     //res.send(`Hello Cat with id ${req.params.id}!`);
