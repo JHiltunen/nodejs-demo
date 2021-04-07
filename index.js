16cac1f0d0f8fd21cf0eaee47812d45adfafec38
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-07 14:07:06 +0300

    Added authentication

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 'use strict';
-
 const express = require('express');
+const cors = require('cors');
+const catRoute = require('./routes/catRouter');
+const userRoute = require('./routes/userRouter');
+const passport = require('./utils/pass');
+const authRoute = require('./routes/authRoute');
 const app = express();
 const port = 3000;
-const catRouter = require('./routes/catRouter');
-const userRouter = require('./routes/userRouter');
+
+app.use(cors());
+
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 app.use(express.static('uploads'));
-app.use(express.json()); // for parsing application/json
-//app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use('/cat', catRouter);
-app.use('/user', userRouter);
+// routes
+app.use('/auth', authRoute);
+app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
+app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/userModel.js
+++ models/userModel.js
@@ -40,9 +40,22 @@ const insertUser = async (user) => {
   return row.insertId;
 };
 
+const getUserLogin = async (params) => {
+  try {
+    console.log(params);
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_user WHERE email = ?;',
+        params);
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
+
 module.exports = {
   getAllUsers,
   getAllUsersSort,
   getUser,
   insertUser,
+  getUserLogin,
 };
