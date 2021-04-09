a126a6d48cbddda515baf2de9fe01cc1e9153060
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-04-09 10:55:06 +0300

    index.js with https ready for both localhost and production servers

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict';
 const express = require('express');
+
 const cors = require('cors');
 const catRoute = require('./routes/catRouter');
 const userRoute = require('./routes/userRouter');
 const passport = require('./utils/pass');
 const authRoute = require('./routes/authRoute');
 const app = express();
-const port = 3000;
+const port = process.env.HTTP_PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./production')(app, process.env.PORT);
+} else {
+  require('./utils/localhost')(app, process.env.HTTPS_PORT || 8000, port);
+}
+app.get('/', (req, res) => {
+  res.send('Hello Secure World!');
+});
+
 
 app.use(cors());
 
@@ -19,5 +31,7 @@ app.use(express.static('uploads'));
 app.use('/auth', authRoute);
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
+/*
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));*/
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
